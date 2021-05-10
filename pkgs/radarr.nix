047ba92bebfdb9bae7e0e203e79853dbdcd84a02
Winter <winter@winter.cafe>
2021-05-09 22:06:24 -0400

    radarr: remove fhsuserenv

--- pkgs/radarr.nix
+++ pkgs/radarr.nix
@@ -1,40 +1,31 @@
 manifest:
-{ lib, buildFHSUserEnv, stdenv, fetchurl, makeWrapper, autoPatchelfHook, zlib
-, lttngUst, curl, icu, openssl, sqlite, libmediainfo }:
-let
-  pkg = stdenv.mkDerivation rec {
-    name = "radarr";
-    version = manifest.version;
+{ lib, stdenv, fetchurl, makeWrapper, autoPatchelfHook, zlib, lttngUst, curl
+, icu, openssl, sqlite, libmediainfo }:
+stdenv.mkDerivation rec {
+  name = "radarr";
+  version = manifest.version;
 
-    src = fetchurl {
-      name = "radarr.tar.gz"; # see nixpkgs/107515
-      inherit (manifest) url sha256;
-    };
-
-    nativeBuildInputs = [ makeWrapper autoPatchelfHook ];
-
-    buildInputs = [ stdenv.cc.cc zlib lttngUst curl ];
-
-    installPhase = ''
-      mkdir -p $out/bin $out/radarr/bin
-      cp -r * $out/radarr/bin
-      echo "${''
-        PackageAuthor=[kaidame](https://github.com/winterqt/kaidame)
-        UpdateMethod=External
-        UpdateMethodMessage=[check the commit log](https://github.com/winterqt/kaidame/commits/main) and run \`nix flake update\`
-        Branch=${manifest.branch}
-        PackageVersion=${version}
-      ''}" > $out/radarr/package_info
-      makeWrapper $out/radarr/bin/Radarr $out/bin/Radarr --prefix LD_LIBRARY_PATH : "${
-        lib.makeLibraryPath [ icu openssl sqlite libmediainfo ]
-      }"
-    '';
+  src = fetchurl {
+    name = "radarr.tar.gz"; # see nixpkgs/107515
+    inherit (manifest) url sha256;
   };
-  # an FHS is used to use the musl detection, will remove when the fix hits stable
-in buildFHSUserEnv {
-  name = "Radarr";
 
-  targetPkgs = pkgs: [ pkg ];
+  nativeBuildInputs = [ makeWrapper autoPatchelfHook ];
 
-  runScript = "/bin/Radarr";
+  buildInputs = [ stdenv.cc.cc zlib lttngUst curl ];
+
+  installPhase = ''
+    mkdir -p $out/bin $out/radarr/bin
+    cp -r * $out/radarr/bin
+    echo "${''
+      PackageAuthor=[kaidame](https://github.com/winterqt/kaidame)
+      UpdateMethod=External
+      UpdateMethodMessage=[check the commit log](https://github.com/winterqt/kaidame/commits/main) and run \`nix flake update\`
+      Branch=${manifest.branch}
+      PackageVersion=${version}
+    ''}" > $out/radarr/package_info
+    makeWrapper $out/radarr/bin/Radarr $out/bin/Radarr --prefix LD_LIBRARY_PATH : "${
+      lib.makeLibraryPath [ icu openssl sqlite libmediainfo ]
+    }"
+  '';
 }
