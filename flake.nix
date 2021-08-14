2057c3f1bcf2837e0791a9e3a9a5186b170198fe
Winter <winter@winter.cafe>
2021-08-14 15:59:41 -0400

    only expose packages to systems that support them

--- flake.nix
+++ flake.nix
@@ -3,35 +3,38 @@
   inputs.flake-utils.url = "github:winterqt/flake-utils";
 
   outputs = { self, nixpkgs, flake-utils }:
-    flake-utils.lib.eachDefaultSystem
-      (system:
-        let
-          pkgs = nixpkgs.legacyPackages.${system};
-          versions = builtins.fromJSON (builtins.readFile ./versions.json);
-          mkSonarr = import ./pkgs/sonarr.nix;
-          mkRadarr = import ./pkgs/radarr.nix;
-          mkJellyfin = import ./pkgs/jellyfin.nix;
-        in
+    nixpkgs.lib.recursiveUpdate
+      (flake-utils.lib.eachSystem [ "x86_64-linux" ]
+        (system:
+          let
+            pkgs = nixpkgs.legacyPackages.${system};
+            versions = builtins.fromJSON (builtins.readFile ./versions.json);
+            mkSonarr = import ./pkgs/sonarr.nix;
+            mkRadarr = import ./pkgs/radarr.nix;
+            mkJellyfin = import ./pkgs/jellyfin.nix;
+          in
 
+          {
+            packages = {
+              sonarr = pkgs.callPackage (mkSonarr versions.sonarr.v3-stable) { };
+              sonarr-nightly = pkgs.callPackage (mkSonarr versions.sonarr.v3-nightly) { };
+              sonarr-preview = pkgs.callPackage (mkSonarr versions.sonarr.v3-preview) { };
+
+              radarr = pkgs.callPackage (mkRadarr versions.radarr.master) { };
+              radarr-develop = pkgs.callPackage (mkRadarr versions.radarr.develop) { };
+              radarr-nightly = pkgs.callPackage (mkRadarr versions.radarr.nightly) { };
+
+              jellyfin = pkgs.callPackage (mkJellyfin versions.jellyfin.stable) { };
+              jellyfin-rc = pkgs.callPackage (mkJellyfin versions.jellyfin.stable-rc) { };
+            };
+          }))
+      (flake-utils.lib.eachDefaultSystem (system:
+        let pkgs = nixpkgs.legacyPackages.${system}; in
         {
-          packages = {
-            sonarr = pkgs.callPackage (mkSonarr versions.sonarr.v3-stable) { };
-            sonarr-nightly = pkgs.callPackage (mkSonarr versions.sonarr.v3-nightly) { };
-            sonarr-preview = pkgs.callPackage (mkSonarr versions.sonarr.v3-preview) { };
-
-            radarr = pkgs.callPackage (mkRadarr versions.radarr.master) { };
-            radarr-develop = pkgs.callPackage (mkRadarr versions.radarr.develop) { };
-            radarr-nightly = pkgs.callPackage (mkRadarr versions.radarr.nightly) { };
-
-            jellyfin = pkgs.callPackage (mkJellyfin versions.jellyfin.stable) { };
-            jellyfin-rc = pkgs.callPackage (mkJellyfin versions.jellyfin.stable-rc) { };
-
-            updater = pkgs.callPackage (import ./pkgs/updater.nix)
-              {
-                Security = pkgs.darwin.apple_sdk.frameworks.Security;
-              };
-          };
-
+          packages.updater = pkgs.callPackage (import ./pkgs/updater.nix)
+            {
+              Security = pkgs.darwin.apple_sdk.frameworks.Security;
+            };
           devShell = pkgs.mkShell {
             nativeBuildInputs = with pkgs; [
               rustc
@@ -40,5 +43,5 @@
               pkg-config
             ] ++ pkgs.lib.optionals pkgs.stdenv.isDarwin (with pkgs; [ libiconv darwin.apple_sdk.frameworks.Security ]);
           };
-        });
+        }));
 }
